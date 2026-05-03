a4fc5653d4834d40d3bb3391837f4485be9e3973
kacperb0rowski <kacpi1218@tlen.pl>
2026-05-03 19:20:35 +0200

    added gitignore, .env file and API key variable

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import openai
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 email_content = input("Hello, how can I help you today? ")
 print(email_content)
